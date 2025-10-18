d7e543116cde654aa69224fb5fa39d696beaebfe
cj1680 <johnsonc1614@gmail.com>
2025-10-18 14:22:36 -0500

    Create playwright.config.ts

---
+++ client/playwright.config.ts
@@ -0,0 +1,12 @@
+// playwright.config.ts
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'src/tests',  // points to your test files
+  timeout: 30 * 1000,
+  retries: 0,
+  reporter: [['list']],
+  use: {
+    headless: true,
+  },
+});
