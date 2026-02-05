59dd185e035a3a5b6491509c4a29fd755a2e7486
cmmartin14 <corinna.m.martin@gmail.com>
2026-02-05 12:14:38 -0600

    Fixed drawing toolbar placement

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -301,7 +301,7 @@ const Canvas = () => {
       {editMode && (
         <div
           data-testid="edit-window"
-          className="absolute top-4 left-4 mt-10 bg-white rounded-lg shadow-lg p-3 border z-40"
+          className="absolute top-0 left-4 mt-5 bg-white rounded-lg shadow-lg p-3 border z-40"
         >
           <div className="flex gap-2">
             {/* Circle */}
