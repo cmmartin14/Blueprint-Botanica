f3a76b4e36d56d867f261b52a3469996a3979ffb
cj1680 <johnsonc1614@gmail.com>
2026-02-26 01:56:47 -0600

    roound 4

--- client/prisma.config.ts
+++ client/prisma.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
-  engine: "classic",
+  
   datasource: {
-    url: env("DATABASE_URL"),
-  },
+    url: process.env.DATABASE_URL!
+  }
 });
